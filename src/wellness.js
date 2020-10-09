03a8bec49558fe7bba79e07dd40c6c2e05fbb4c7
Alberto Lejárraga Rubio <alejarragar@correo.ugr.es>
2020-10-09 14:54:29 +0200

    Se crea la clase Wellness #10

---
+++ src/wellness.js
@@ -0,0 +1,14 @@
+class Wellness {
+	constructor (fecha, idJugador, fatiga, danoMuscular, calidadSueno, nutricionHidratacion, estadoEmocional){
+	}
+	get fecha(){}
+	get idJugador(){}
+	get fatiga(){}
+	get danoMuscular(){}
+	get calidadSueno(){}
+	get nutricionHidratacion(){}
+	get estadoEmocional(){}		
+	anomalias(){
+		//devuelve los valores anómalos, o nulo en caso de que no los hubiera
+		}
+}
