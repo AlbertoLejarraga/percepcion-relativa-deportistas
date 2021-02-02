c29f7a9d3688c56be0526f78de9a5676656e85f6
= <alejarragar@correo.ugr.es>
2021-02-02 14:01:31 +0100

    Se enlaza el código con las historias de usuario correspondientes

--- src/rpe.js
+++ src/rpe.js
@@ -1,8 +1,20 @@
 class Rpe{
 	constructor (idJugador, fecha, turno, valorRPE){
 	}
+	//getters
 	get idJugador (){}
 	get fecha (){}
 	get turno (){}
 	get valorRPE (){}
+	//setters
+	set idJugador (){}
+	set fecha (){}
+	set turno (){}
+	set valorRPE (){}
+	//HU5: Rellenar encuesta
+	rellenarEncuesta(idJugador, turno, valorRPE){}
+	//HU4: Obtener las encuestas de un jugador de la temporada
+	obtenerEncuestasJugador(idJugador){}
+	//HU3: Obtener las encuestas de todos los jugadores de una sesión concreta
+	obtenerEncuestasDia(fecha, turno){}
 }
--- src/wellness.js
+++ src/wellness.js
@@ -1,14 +1,28 @@
+//Clase relacionada con las encuestas wellness
 class Wellness {
 	constructor (fecha, idJugador, fatiga, danoMuscular, calidadSueno, nutricionHidratacion, estadoEmocional){
 	}
+	//getters
 	get fecha(){}
 	get idJugador(){}
 	get fatiga(){}
 	get danoMuscular(){}
 	get calidadSueno(){}
 	get nutricionHidratacion(){}
-	get estadoEmocional(){}		
-	anomalias(){
-		//devuelve los valores anómalos, o nulo en caso de que no los hubiera
-		}
+	get estadoEmocional(){}
+	//setters
+	set fecha(){}
+	set idJugador(){}
+	set fatiga(){}
+	set danoMuscular(){}
+	set calidadSueno(){}
+	set nutricionHidratacion(){}
+	set estadoEmocional(){}
+
+	//HU6: Rellenar encuesta
+	rellenarEncuesta(idJugador, fatiga, danoMuscular, calidadSueno, nutricionHidratacion, estadoEmocional){}
+	//HU2: Obtener las encuestas de un jugador de la temporada
+	obtenerEncuestasJugador(idJugador){}
+	//HU1: Obtener las encuestas de todos los jugadores de un día concreto
+	obtenerEncuestasDia(fecha){}
 }
