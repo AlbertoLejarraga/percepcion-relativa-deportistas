b51b0c1955a43a95a010dd985d670147192c7b92
= <alejarragar@correo.ugr.es>
2021-02-04 21:54:22 +0100

    se completan funciones básicas de rpe y el as_string (#15 y #16 de rpe)

--- src/rpe.js
+++ src/rpe.js
@@ -1,20 +1,41 @@
 class Rpe{
 	constructor (idJugador, fecha, turno, valorRPE){
+		this._idJugador = idJugador
+		this._fecha = fecha
+		this._turno = turno
+		this._valor = valorRPE
 	}
 	//getters
-	get idJugador (){}
-	get fecha (){}
-	get turno (){}
-	get valorRPE (){}
+	get idJugador (){return this._idJugador}
+	get fecha (){return this._fecha}
+	get turno (){return this._turno}
+	get valor (){return this._valor}
 	//setters
-	set idJugador (){}
-	set fecha (){}
-	set turno (){}
-	set valorRPE (){}
+	set idJugador (idJug){
+		this._idJugador = idJug
+	}
+	set fecha (fecha){
+		this._fecha = fecha
+	}
+	set turno (turno){
+		this._turno = turno
+	}
+	set valor (valor){
+		this._valor = valorRPE
+	}
+	as_string(){
+		return `{idJugador: ${this.idJugador}, fecha: ${this.fecha}, turno: ${this.turno}, valor: ${this.valor}}`
+	}
 	//HU5: Rellenar encuesta
 	rellenarEncuesta(idJugador, turno, valorRPE){}
 	//HU4: Obtener las encuestas de un jugador de la temporada
 	obtenerEncuestasJugador(idJugador){}
 	//HU3: Obtener las encuestas de todos los jugadores de una sesión concreta
 	obtenerEncuestasDia(fecha, turno){}
+
+
+}
+
+module.exports = {
+	Rpe
 }
