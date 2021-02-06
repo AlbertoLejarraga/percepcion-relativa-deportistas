5a218a18fedb4627e40b249b32cae8e640c13bf2
= <alejarragar@correo.ugr.es>
2021-02-06 17:42:52 +0100

    se generan y se prueban los test de de wellness closes #19

--- tests/principal.js
+++ tests/principal.js
@@ -38,3 +38,46 @@ describe('RPE', function(){
     })
   });
 });
+
+
+describe('Wellness', function(){
+  describe('Creación de objeto', function(){
+    it('Genera un objeto', function(){
+      expect(wellTest).to.be.an('object');
+    });
+  });
+  describe('Rellenar encuesta', function(){
+    it('Devuelve false al rellenarla sin valores', function(){
+      r = wellTest.rellenarEncuesta("", "", "")
+      expect(r).to.be.false;
+    });
+    it('Devuelve false al poner un valor menor de 1 en alguno de los campos', function(){
+      r = wellTest.rellenarEncuesta("123456", 0, 1, 2, 3, 4)
+      expect(r).to.be.false;
+      r = wellTest.rellenarEncuesta("123456", 4, 1, -9, 3, 4)
+      expect(r).to.be.false;
+      r = wellTest.rellenarEncuesta("123456", 5, 1, 2, -1, 4)
+      expect(r).to.be.false;
+    });
+    it('Devuelve false al poner un valor mayor de 7 en alguno de los campos', function(){
+      r = wellTest.rellenarEncuesta("123456", 5, 11, 2, 3, 4)
+      expect(r).to.be.false;
+      r = wellTest.rellenarEncuesta("123456", 4, 1, 5, 15, 4)
+      expect(r).to.be.false;
+      r = wellTest.rellenarEncuesta("123456", 5, 1, 16, 5, 4)
+      expect(r).to.be.false;
+    });
+    it('Devuelve un objecto correcto al pasar valores correctos', function(){
+      r = wellTest.rellenarEncuesta("123456", 5, 6, 2, 3, 4)
+      expect(r).to.be.true;
+      expect(wellTest).to.be.an('object');
+      expect(wellTest.idJugador).to.equal("123456");
+      expect(wellTest.fatiga).to.equal(5);
+      expect(wellTest.danoMuscular).to.equal(6);
+      expect(wellTest.calidadSueno).to.equal(2);
+      expect(wellTest.nutricionHidratacion).to.equal(3);
+      expect(wellTest.estadoEmocional).to.equal(4);
+
+    })
+  });
+});
