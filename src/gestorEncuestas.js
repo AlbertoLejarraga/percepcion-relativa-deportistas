45ae64618ddafbc11e566bc8eebfaceaa1dca060
= <alejarragar@correo.ugr.es>
2021-02-12 21:01:04 +0100

    Se sube el gestor tras pasar los tests. #4 #5 #6 #7 #8 #9 closes #30

--- src/gestorEncuestas.js
+++ src/gestorEncuestas.js
@@ -1,79 +1,98 @@
-var rpe = require('rpe.js')
-var wellness = require('wellness.js')
-var model = require('model.js')
-class gestorEncuestas{
-  constructor(){
+var rpe = require('./rpe.js')
+var wellness = require('./wellness.js')
+var model = require('./model.js')
+class GestorEncuestas{
+  constructor (){
     this._model = new model.Model()
+  }
+  get [Symbol.toStringTag]() {
+    return 'GestorEncuestas';
   }
   ////////////////////////////////////////////// RPE /////////////////////////////////////
   //HU3: rpe de un día
-  obtenerRPE(idJugador, fecha, turno="m"){
+  obtenerRPESesion(idJugador, fecha, turno="m"){
     //A partir del idJugador, la fecha y el turno se realiza una conexión a la bbdd para obtener la encuesta de esa sesion
     //se comprueba que la fecha y el turno sean correctos
-    f = new Date(fecha)
+    let f = new Date(fecha)
     if (! fecha instanceof Date || isNaN(f) || (turno !== "m" && turno !== "t")){
       return "Fecha o turno incorrectos"
     }
-    rpeSesion = this._model.obtenerRPESesion(idJugador, fecha, turno)
+    let rpeSesion = this._model.obtenerRPESesion(idJugador, fecha, turno)
     if (rpeSesion !== {}){
-      return new rpe.Rpe().rellenarEncuesta(idJugador, fecha, turno, rpeSesion)
+      let r = new rpe.Rpe()
+      r.rellenarEncuesta(idJugador, fecha, turno, rpeSesion)
+      return r
     }else{
       return "No existe encuesta para ese jugador/día/turno"
     }
   }
   //HU4: todos los rpe del jugador, con un límite
-  obtenerRPE(idJugador, limite = 100){
+  obtenerRPETemp(idJugador, limite = 10){
     //se devuelven todas las encuestas del jugador
-    datos = this._model.obtenerRPETotal(idJugador, limite)
-    encuestas = []
-    for(let i=0;i<datos.length();i++){
-      encuestas.push(rpe.new Rpe().rellenarEncuesta(datos["idJugador"], datos["fecha"], datos["turno"], datos["valor"]))
+    var datos = this._model.obtenerRPETotal(idJugador, limite)
+    var encuestas = []
+    for(let i=0;i<datos.length;i++){
+      let rpeActual = new rpe.Rpe()
+      rpeActual.rellenarEncuesta(datos[i]["idJugador"], datos[i]["fecha"], datos[i]["turno"], datos[i]["valor"]);
+      encuestas.push(rpeActual)
     }
     return encuestas
   }
   //HU5: Rellenar encuesta rpe
   nuevoRPE(idJugador, fecha, turno, rpeSesion){
-    encuesta = new rpe.Rpe().rellenarEncuesta(idJugador, fecha, turno, rpeSesion)
-    if (encuesta){
-      return model.addRpe(encuesta.as_dict())
-    }else{
-      return "datos erroneos"
+    if ( fecha instanceof Date && ! isNaN(fecha) && (turno === "m" || turno === "t") && (rpeSesion >= 1 || rpeSesion <= 10)){
+      let encuesta = new rpe.Rpe()
+      encuesta.rellenarEncuesta(idJugador, fecha, turno, rpeSesion)
+      return this._model.addRpe(encuesta.as_dict())
     }
+    return "Fecha o turno incorrectos"
   }
   /////////////////////////////////////////////// WELLNESS ////////////////////////////////
   // HU1: Wellness de un día
-  obtenerWellness(idJugador, fecha){
+  obtenerWellnessDia(idJugador, fecha){
     //A partir del idJugador y la fecha se realiza una conexión a la bbdd para obtener la encuesta de ese día
     //se comprueba que la fecha y el turno sean correctos
-    f = new Date(fecha)
-    if (! fecha instanceof Date || isNaN(f)){
-      return "Fecha o turno incorrectos"
+    let f = new Date(fecha)
+    if (! fecha instanceof Date || isNaN(fecha)){
+      return "Fecha incorrecta"
     }
-    wellnessDia = this._model.obtenerWellnessDia(idJugador, fecha)
+    let wellnessDia = this._model.obtenerWellnessDia(idJugador, fecha)
     if (wellnessDia !== {}){
-      return new wellness.Wellness().rellenarEncuesta(idJugador, fecha, wellnessDia["fatiga"], wellnessDia["danoMuscular"], wellnessDia["calidadSueno"], wellnessDia["nutricionHidratacion"], wellnessDia["estadoEmocional"])
+      let w = new wellness.Wellness()
+      w.rellenarEncuesta(idJugador, fecha, wellnessDia["fatiga"], wellnessDia["danoMuscular"], wellnessDia["calidadSueno"], wellnessDia["nutricionHidratacion"], wellnessDia["estadoEmocional"])
+      return w
     }else{
       return "No existe encuesta para ese jugador/día/turno"
     }
   }
   // HU2: Wellness de la temporada
-  obtenerWellness(idJugador, limite = 100){
+  obtenerWellnessTemp(idJugador, limite = 10){
     //se devuelven todas las encuestas del jugador
-    datos = this._model.obtenerWellnessTotal(idJugador, limite)
-    encuestas = []
-    for(let i=0;i<datos.length();i++){
-      encuestas.push(rpe.new Wellness().rellenarEncuesta(datos["idJugador"], datos["fecha"], datos["fatiga"], datos["danoMuscular"], datos["calidadSueno"], datos["nutricionHidratacion"], datos["estadoEmocional"]))
+    let datos = this._model.obtenerWellnessTotal(idJugador, limite)
+    let encuestas = []
+    for(let i=0;i<datos.length;i++){
+      let wellActual = new wellness.Wellness()
+      wellActual.rellenarEncuesta(datos[i]["idJugador"], datos[i]["fecha"], datos[i]["fatiga"], datos[i]["danoMuscular"], datos[i]["calidadSueno"], datos[i]["nutricionHidratacion"], datos[i]["estadoEmocional"])
+      encuestas.push(wellActual)
     }
     return encuestas
   }
   //HU6: Rellenar encuesta wellness
   nuevoWellness(idJugador, fecha, fatiga, danoMuscular, calidadSueno, nutricionHidratacion, estadoEmocional){
-    encuesta = new rpe.Rpe().rellenarEncuesta(idJugador, fecha, fatiga, danoMuscular, calidadSueno, nutricionHidratacion, estadoEmocional)
-    if (encuesta){
-      return model.addWellness(encuesta.as_dict())
-    }else{
-      return "datos erroneos"
+    if (! fecha instanceof Date || isNaN(fecha) ||
+    (fatiga < 1 || fatiga > 7) ||
+    (danoMuscular < 1 || danoMuscular > 7) ||
+    (calidadSueno < 1 || calidadSueno > 7) ||
+    (nutricionHidratacion < 1 || nutricionHidratacion > 7) ||
+    (estadoEmocional < 1 || estadoEmocional > 7)){
+      return "Dato incorrecto"
     }
+    let encuesta = new rpe.Rpe()
+    encuesta.rellenarEncuesta(idJugador, fecha, fatiga, danoMuscular, calidadSueno, nutricionHidratacion, estadoEmocional)
+    return this._model.addWellness(encuesta.as_dict())
   }
-  
+
+}
+module.exports = {
+	GestorEncuestas
 }
